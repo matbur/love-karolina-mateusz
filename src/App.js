6e037765e1d22c5b7b39183dcfbfc133d06ea976
matbur <mateusz@burniak.pl>
2019-04-24 15:46:19 +0200

    Use cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert } from 'react-bootstrap';
+import { Alert, Card, CardDeck } from 'react-bootstrap';
 import moment from 'moment';
 // eslint-disable-next-line no-unused-vars
 import * as pl from 'moment/locale/pl';
@@ -30,27 +30,70 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Alert variant="info">{`${now.format(format + ' - HH:mm:ss')}`}</Alert>
-        <Date text="Poznanie" value="2018-07-23" now={now} />
-        <Date text="Związek" value="2018-09-14" now={now} />
-        <Date text="Vegas" value="2020-08-22" now={now} />
+        <Card>
+          <Card.Header>Karolina i Mateusz</Card.Header>
+          <Card.Body>
+            <CardDeck>
+              <Date
+                header="Piersze spotkanie"
+                value="2018-07-23"
+                now={now}
+                src="kajaki.jpg"
+              />
+              <Date
+                header="Początek związku"
+                value="2018-09-14"
+                now={now}
+                src="giewont.jpg"
+              />
+              <Date
+                header="Vegas"
+                value="2020-08-22"
+                now={now}
+                src="vegas.jpg"
+              />
+            </CardDeck>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">{`Aktualny czas ${now.format(
+              format + ' - HH:mm:ss'
+            )}`}</small>
+          </Card.Footer>
+        </Card>
       </div>
     );
   }
 }
 
 class Date extends Component {
-  beforeAfter = days => (days > 0 ? `za ${days} dni` : `${-days} dni temu`);
+  beforeAfter = (val, s) => (val > 0 ? `za ${val} ${s}` : `${-val} ${s} temu`);
 
   render() {
-    const { now, text, value } = this.props;
+    const { header, src, now, value } = this.props;
     const date = moment(value);
-    const diff = date.diff(now, 'days');
-
     return (
-      <Alert variant="primary">
-        {`${text}: ${date.format(format)} (${this.beforeAfter(diff)})`}
-      </Alert>
+      <Card bg="light">
+        <Card.Img variant="top" src={src} />
+        <Card.Body>
+          <Card.Title>{header}</Card.Title>
+          <Card.Text>
+            <ul>
+              <li>{`${this.beforeAfter(date.diff(now, 'days'), 'dni')}`}</li>
+              <li>{`${this.beforeAfter(
+                date.diff(now, 'weeks'),
+                'tygodnie'
+              )}`}</li>
+              <li>{`${this.beforeAfter(
+                date.diff(now, 'months'),
+                'miesięcy'
+              )}`}</li>
+            </ul>
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">{`${date.format(format)}`}</small>
+        </Card.Footer>
+      </Card>
     );
   }
 }
