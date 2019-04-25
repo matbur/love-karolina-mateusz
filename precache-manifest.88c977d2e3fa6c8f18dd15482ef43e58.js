386a73e25919f0691c8b9f126b6e93a4a776634f
matbur <mateusz@burniak.pl>
2019-04-25 14:54:51 +0200

    Updates

---
+++ precache-manifest.88c977d2e3fa6c8f18dd15482ef43e58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752255084766fd10a5ae4401377845d3",
+    "url": "/love-karolina-mateusz/index.html"
+  },
+  {
+    "revision": "8a97f7aaf1efabbfb4f6",
+    "url": "/love-karolina-mateusz/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "fe372d7fe430a1fd26dc",
+    "url": "/love-karolina-mateusz/static/js/2.88c29b05.chunk.js"
+  },
+  {
+    "revision": "8a97f7aaf1efabbfb4f6",
+    "url": "/love-karolina-mateusz/static/js/main.9df89304.chunk.js"
+  },
+  {
+    "revision": "4a82ea306bfecce109fc",
+    "url": "/love-karolina-mateusz/static/js/runtime~main.e38b5a38.js"
+  }
+]);
