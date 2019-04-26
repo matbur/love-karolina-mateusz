794970e69a428658f7be4589b4eaaa50da9fa0eb
matbur <mateusz@burniak.pl>
2019-04-26 08:43:39 +0200

    Refactor 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import moment from 'moment';
 import 'moment/locale/pl';
 import { equals, takeLast } from 'ramda';
 
+import { calculateDays, beforeAfter, calculateWeeks, calculateMonths } from './utils/date';
+
 moment.locale('pl');
 
 const format = 'ddd, D MMM Y';
@@ -14,26 +16,18 @@ interface AppState {
   isHidden: boolean;
 }
 class App extends Component<{}, AppState> {
-  t: NodeJS.Timeout;
+  public state = {
+    now: moment(),
+    clicked: [],
+    isHidden: true,
+  };
 
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
-      now: moment(),
-      clicked: [],
-      isHidden: true,
-    };
-
-    this.t = setInterval(() => {
+  componentDidMount() {
+    setInterval(() => {
       this.setState({
         now: moment(),
       });
     }, 100);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.t);
   }
 
   clicked = (n: number) => () => {
@@ -59,14 +53,14 @@ class App extends Component<{}, AppState> {
           </Card.Header>
           <Card.Body>
             <CardDeck>
-              <Date
+              <Event
                 header="Piersze spotkanie"
                 value="2018-07-23"
                 now={today}
                 src="kajaki.jpg"
                 handleClick={this.clicked(1)}
               />
-              <Date
+              <Event
                 header="Początek związku"
                 value="2018-09-14"
                 now={today}
@@ -74,7 +68,7 @@ class App extends Component<{}, AppState> {
                 handleClick={this.clicked(2)}
               />
               {isHidden ? null : (
-                <Date
+                <Event
                   header="Vegas"
                   value="2020-08-22"
                   now={today}
@@ -95,105 +89,95 @@ class App extends Component<{}, AppState> {
   }
 }
 
-interface DateProps {
+interface PartsProps {
+  date: moment.Moment;
+  today: moment.Moment;
+}
+
+const Days: React.FC<PartsProps> = ({ date, today }) => {
+  const days = calculateDays(date, today);
+
+  return (
+    <li>
+      {days === 0
+        ? 'to już dzisiaj'
+        : beforeAfter(days, 'dni')
+      }
+    </li>
+  );
+};
+
+const Weeks: React.FC<PartsProps> = ({ date, today }) => {
+  const { weeks, days } = calculateWeeks(date, today);
+
+  return weeks === 0
+    ? null
+    : (
+      <li>
+        {beforeAfter(weeks, 'tyg.')}
+        {days === 0
+          ? null
+          : (
+            <>
+              <br />
+              {`i ${Math.abs(days)} dni`}
+            </>
+          )}
+      </li>
+    );
+};
+
+const Months: React.FC<PartsProps> = ({ date, today }) => {
+  const { months, days } = calculateMonths(date, today);
+
+  return months === 0
+    ? null
+    : (
+      <li>
+        {beforeAfter(months, 'm-cy')}
+        {days === 0
+          ? null
+          : (
+            <>
+              <br />
+              {`i ${Math.abs(days)} dni`}
+            </>
+          )}
+      </li>
+    );
+};
+
+interface EventProps {
   header: string;
   value: string;
   now: moment.Moment;
   src: string;
   handleClick: () => void;
 }
-class Date extends Component<DateProps, {}> {
-  beforeAfter = (val: number, s: string) =>
-    val > 0 ? `za ${val} ${s}` : `${-val} ${s} temu`;
 
-  calculateDays = (date: moment.Moment, now: moment.Moment) => {
-    const diffDays = date.diff(now, 'days');
+const Event: React.FC<EventProps> = ({ header, value, now, src, handleClick }) => {
+  const date = moment(value);
 
-    return (
-      <li>
-        {diffDays === 0 ? 'to już dzisiaj' : this.beforeAfter(diffDays, 'dni')}
-      </li>
-    );
-  };
-
-  calculateWeeks = (date: moment.Moment, now: moment.Moment) => {
-    const diff = date.diff(now, 'days');
-    const diffWeeks = (diff / 7) >> 0;
-    const diffDays = diff % 7;
-
-    return diffWeeks === 0 ? null : (
-      <li>
-        {this.beforeAfter(diffWeeks, 'tyg.')}
-        {diffDays === 0 ? null : (
-          <>
-            <br />
-            {`i ${Math.abs(diffDays)} dni`}
-          </>
-        )}
-      </li>
-    );
-  };
-
-  calculateMonths = (date: moment.Moment, now: moment.Moment) => {
-    const diffMonths = date.diff(now, 'months');
-    const diff = date.diff(now, 'days');
-    const dd = date.date();
-    const nd = now.date();
-
-    let diffDays = 0;
-
-    if (diff < 0) {
-      if (dd < nd) {
-        diffDays = dd - nd;
-      } else if (dd > nd) {
-        diffDays = dd - nd - date.clone().endOf('month').date();
-      }
-    } else if (diff > 0) {
-      if (dd < nd) {
-        diffDays = dd - nd + now.clone().endOf('month').date();
-      } else if (dd > nd) {
-        diffDays = dd - nd;
-      }
-    }
-
-    return diffMonths === 0 ? null : (
-      <li>
-        {this.beforeAfter(diffMonths, 'm-cy')}
-        {diffDays === 0 ? null : (
-          <>
-            <br />
-            {`i ${Math.abs(diffDays)} dni`}
-          </>
-        )}
-      </li>
-    );
-  };
-
-  render() {
-    const { header, src, now, value, handleClick } = this.props;
-    const date = moment(value);
-
-    return (
-      <Card
-        bg="light"
-        style={{ minWidth: '11.09rem', marginBottom: 10 }}
-        onClick={handleClick}
-      >
-        <Card.Img variant="top" src={src} />
-        <Card.Body>
-          <Card.Title>{header}</Card.Title>
-          <ul>
-            {this.calculateDays(date, now)}
-            {this.calculateWeeks(date, now)}
-            {this.calculateMonths(date, now)}
-          </ul>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">{`${date.format(format)}`}</small>
-        </Card.Footer>
-      </Card>
-    );
-  }
-}
+  return (
+    <Card
+      bg="light"
+      style={{ minWidth: '11.09rem', marginBottom: 10 }}
+      onClick={handleClick}
+    >
+      <Card.Img variant="top" src={src} />
+      <Card.Body>
+        <Card.Title>{header}</Card.Title>
+        <ul>
+          <Days date={date} today={now} />
+          <Weeks date={date} today={now} />
+          <Months date={date} today={now} />
+        </ul>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">{`${date.format(format)}`}</small>
+      </Card.Footer>
+    </Card>
+  );
+};
 
 export default App;
