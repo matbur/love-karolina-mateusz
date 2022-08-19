1062a9a2c24fa9b96fc999d0559b5eeebefd83da
Mateusz Burniak <mateusz.burniak@apilia.pl>
2022-08-19 08:29:50 +0200

    add typed component

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 
 import './style.css';
 
-const App: React.FC = () => <div>1!</div>
+type HelloProps = {
+    to: string,
+    howMany?: number,
+}
+
+const Hello: React.FC<HelloProps> = ({ to, howMany = 3 }) => <div>Hello, {to} ({howMany})</div>
+
+const App: React.FC<{}> = () => <div>asd1!
+    <Hello to="world" />
+</div>
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')!
